c753de74ffd2c44702e5795cae2abee066c6304c
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2020-11-29 15:14:20 +0330

    updates

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,25 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/Route::get('/', function (\Codedge\Updater\UpdaterManager $updater) {
 
-Route::get('/', function () {
-    return view('welcome');
+    // Check if new version is available
+    if($updater->source()->isNewVersionAvailable()) {
+
+        // Get the current installed version
+        echo $updater->source()->getVersionInstalled();
+
+        // Get the new version available
+        $versionAvailable = $updater->source()->getVersionAvailable();
+
+        // Create a release
+        $release = $updater->source()->fetch($versionAvailable);
+
+        // Run the update process
+        $updater->source()->update($release);
+
+    } else {
+        echo "No new version available.";
+    }
+
 });
